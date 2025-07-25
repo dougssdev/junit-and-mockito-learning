fa64c28376ba44d4bcdcdfc829b69349d257f960
dougssdev <douglassabinosilva@hotmail.com>
2025-07-25 20:27:42 -0300

    test: Testing arrays perfomances

---
+++ src/test/java/br/com/learning/test/ArraysTest.java
@@ -0,0 +1,37 @@
+package br.com.learning.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class ArraysTest {
+
+    @Test
+    @DisplayName("Comparing arrays test")
+    void test(){
+        //Given
+        int[] numbers = {23,5,86,69};
+        int[] expected = {5,23,69,86};
+
+        //When
+        Arrays.sort(numbers);
+
+        //Then
+        assertArrayEquals(expected, numbers);
+    }
+
+    @Test
+    @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance(){
+        int[] numbers = {23,5,86,69};
+
+        for (int i = 0; i < 1000; i++) {
+            numbers[0] = i;
+            Arrays.sort(numbers);
+        }
+    }
+}
