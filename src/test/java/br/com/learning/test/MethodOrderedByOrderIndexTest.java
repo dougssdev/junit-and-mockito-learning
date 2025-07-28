1a55a9abb7602c418f948b341da0e0e70df26171
dougssdev <douglassabinosilva@hotmail.com>
2025-07-28 10:18:44 -0300

    test: Modifying JUnit tests lifecycle

--- src/test/java/br/com/learning/test/MethodOrderedByOrderIndexTest.java
+++ src/test/java/br/com/learning/test/MethodOrderedByOrderIndexTest.java
@@ -2,15 +2,24 @@ package br.com.learning.test;
 
 import org.junit.jupiter.api.*;
 
-@Order(7)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class MethodOrderedByOrder {
+//@Order(7)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderedByOrderIndexTest {
+
+    StringBuilder value = new StringBuilder("");
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("The actual value is: " + value);
+    }
 
     @Test
     @DisplayName("Test A")
     @Order(3)
     void testA(){
         System.out.println("Running test A");
+        value.append("1");
     }
 
 
@@ -19,6 +28,7 @@ public class MethodOrderedByOrder {
     @Order(1)
     void testB(){
         System.out.println("Running test B");
+        value.append("2");
     }
 
 
@@ -27,5 +37,6 @@ public class MethodOrderedByOrder {
     @Order(2)
     void testC(){
         System.out.println("Running test C");
+        value.append("3");
     }
 }
